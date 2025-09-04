ff3684182dffb12bfd77f5151c74e9358519d4c2
mahimathinakaran <pearlinemahima@karunya.edu.in>
2025-09-04 05:44:09 +0530

    Initial commit

---
+++ teams.js
@@ -0,0 +1,26 @@
+// teams.js
+const teams = {
+  1:  { password: "Abraham",  story: "David & Goliath",             set: "1", progressIndex: 0 },
+  2:  { password: "Isaac",  story: "David & Goliath",             set: "2", progressIndex: 0 },
+  3:  { password: "Jacob",  story: "David & Goliath",             set: "3", progressIndex: 0 },
+  4:  { password: "Joseph",  story: "David & Goliath",             set: "4", progressIndex: 0 },
+  5:  { password: "Joshua",  story: "David & Goliath",             set: "5", progressIndex: 0 },
+
+  6:  { password: "Samuel",  story: "Jonah and the Whale",         set: "1", progressIndex: 0 },
+  7:  { password: "Saul",  story: "Jonah and the Whale",         set: "2", progressIndex: 0 },
+  8:  { password: "Solomon",  story: "Jonah and the Whale",         set: "3", progressIndex: 0 },
+  9:  { password: "Elijah",  story: "Jonah and the Whale",         set: "4", progressIndex: 0 },
+ 10:  { password: "Elisha", story: "Jonah and the Whale",         set: "5", progressIndex: 0 },
+
+ 11:  { password: "Esther", story: "Moses and the Red Sea",       set: "1", progressIndex: 0 },
+ 12:  { password: "Daniel", story: "Moses and the Red Sea",       set: "2", progressIndex: 0 },
+ 13:  { password: "Ruth", story: "Moses and the Red Sea",       set: "3", progressIndex: 0 },
+ 14:  { password: "deborah", story: "Moses and the Red Sea",       set: "4", progressIndex: 0 },
+ 15:  { password: "Gideon", story: "Moses and the Red Sea",       set: "5", progressIndex: 0 },
+
+ 16:  { password: "Isaiah", story: "Noah's Ark",                  set: "1", progressIndex: 0 },
+ 17:  { password: "Mary", story: "Noah's Ark",                  set: "2", progressIndex: 0 },
+ 18:  { password: "Paul", story: "Noah's Ark",                  set: "3", progressIndex: 0 },
+ 19:  { password: "Jesus", story: "Noah's Ark",                  set: "4", progressIndex: 0 },
+ 20:  { password: "Timothy", story: "Noah's Ark",                  set: "5", progressIndex: 0 }
+};
