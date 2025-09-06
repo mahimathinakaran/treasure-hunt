175fac19d9c9d8837cc1aeec6aa98cd569eec7b5
mahimathinakaran <pearlinemahima@karunya.edu.in>
2025-09-06 09:01:50 +0530

    Add all HTML pages

--- teams.js
+++ teams.js
@@ -1,26 +1,27 @@
 // teams.js
 const teams = {
-  1:  { password: "Abraham",  story: "David & Goliath",             set: "1", progressIndex: 0 },
-  2:  { password: "Isaac",  story: "David & Goliath",             set: "2", progressIndex: 0 },
-  3:  { password: "Jacob",  story: "David & Goliath",             set: "3", progressIndex: 0 },
-  4:  { password: "Joseph",  story: "David & Goliath",             set: "4", progressIndex: 0 },
-  5:  { password: "Joshua",  story: "David & Goliath",             set: "5", progressIndex: 0 },
+  1:  { password: "48293",  story: "David & Goliath",    set: "1", progressIndex: 0 },
+  2:  { password: "31579",  story: "David & Goliath",    set: "2", progressIndex: 0 },
+  3:  { password: "62840",  story: "David & Goliath",    set: "3", progressIndex: 0 },
+  4:  { password: "94027",  story: "David & Goliath",    set: "4", progressIndex: 0 },
 
-  6:  { password: "Samuel",  story: "Jonah and the Whale",         set: "1", progressIndex: 0 },
-  7:  { password: "Saul",  story: "Jonah and the Whale",         set: "2", progressIndex: 0 },
-  8:  { password: "Solomon",  story: "Jonah and the Whale",         set: "3", progressIndex: 0 },
-  9:  { password: "Elijah",  story: "Jonah and the Whale",         set: "4", progressIndex: 0 },
- 10:  { password: "Elisha", story: "Jonah and the Whale",         set: "5", progressIndex: 0 },
+  5:  { password: "80924",  story: "Jonah and the Whale", set: "1", progressIndex: 0 },
+  6:  { password: "42635",  story: "Jonah and the Whale", set: "2", progressIndex: 0 },
+  7:  { password: "71308",  story: "Jonah and the Whale", set: "3", progressIndex: 0 },
+  8:  { password: "29401",  story: "Jonah and the Whale", set: "4", progressIndex: 0 },
 
- 11:  { password: "Esther", story: "Moses and the Red Sea",       set: "1", progressIndex: 0 },
- 12:  { password: "Daniel", story: "Moses and the Red Sea",       set: "2", progressIndex: 0 },
- 13:  { password: "Ruth", story: "Moses and the Red Sea",       set: "3", progressIndex: 0 },
- 14:  { password: "deborah", story: "Moses and the Red Sea",       set: "4", progressIndex: 0 },
- 15:  { password: "Gideon", story: "Moses and the Red Sea",       set: "5", progressIndex: 0 },
+  9:  { password: "48127",  story: "Moses and the Red Sea", set: "1", progressIndex: 0 },
+  10: { password: "93214",  story: "Moses and the Red Sea", set: "2", progressIndex: 0 },
+  11: { password: "10569",  story: "Moses and the Red Sea", set: "3", progressIndex: 0 },
+  12: { password: "67830",  story: "Moses and the Red Sea", set: "4", progressIndex: 0 },
 
- 16:  { password: "Isaiah", story: "Noah's Ark",                  set: "1", progressIndex: 0 },
- 17:  { password: "Mary", story: "Noah's Ark",                  set: "2", progressIndex: 0 },
- 18:  { password: "Paul", story: "Noah's Ark",                  set: "3", progressIndex: 0 },
- 19:  { password: "Jesus", story: "Noah's Ark",                  set: "4", progressIndex: 0 },
- 20:  { password: "Timothy", story: "Noah's Ark",                  set: "5", progressIndex: 0 }
+  13: { password: "83056",  story: "Noah's Ark",          set: "1", progressIndex: 0 },
+  14: { password: "29487",  story: "Noah's Ark",          set: "2", progressIndex: 0 },
+  15: { password: "56290",  story: "Noah's Ark",          set: "3", progressIndex: 0 },
+  16: { password: "71942",  story: "Noah's Ark",          set: "4", progressIndex: 0 },
+
+  17: { password: "58130",  story: "Miracles of Jesus",   set: "1", progressIndex: 0 },
+  18: { password: "90316",  story: "Miracles of Jesus",   set: "2", progressIndex: 0 },
+  19: { password: "24785",  story: "Miracles of Jesus",   set: "3", progressIndex: 0 },
+  20: { password: "67509",  story: "Miracles of Jesus",   set: "4", progressIndex: 0 }
 };
